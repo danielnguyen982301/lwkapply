26509383d3ee2d226ec2a93e316903d188e7d554
Daniel <danielnguyen982301@gmail.com>
2026-07-08 04:40:15 +0700

    feat(web): Import pinia in main.ts

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 
@@ -7,6 +8,7 @@ import './style.css'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
