604a83f13f21e611d894b8e62b9870be75bcad54
Daniel <danielnguyen982301@gmail.com>
2026-07-09 00:17:55 +0700

    feat(backend): Update config in main.py to set cookies for persistent login

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["Authorization", "Content-Type", "X-CSRF-Token"],
 )
 
 app.include_router(api_router, prefix=settings.API_V1_PREFIX)
