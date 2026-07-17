0aee49ce0dfd293efc38d0ab03f9959b93beb708
Daniel <danielnguyen982301@gmail.com>
2026-07-18 03:54:05 +0700

    feat(web): Create ApplicationStatusTag

---
+++ webapp/src/components/applications/ApplicationStatusTag.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Tag from 'primevue/tag'
+import { APPLICATION_STATUS_LABELS, type ApplicationStatus } from '@/types/application'
+import { applicationStatusSeverity } from '@/lib/application-ui'
+
+defineProps<{ status: ApplicationStatus }>()
+</script>
+
+<template>
+  <Tag :value="APPLICATION_STATUS_LABELS[status]" :severity="applicationStatusSeverity(status)" />
+</template>
