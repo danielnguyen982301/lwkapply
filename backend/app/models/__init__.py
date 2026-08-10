429d7c9b530b8c607c70335f46f8ac8f2cda8c29
Daniel <danielnguyen982301@gmail.com>
2026-08-10 21:58:44 +0700

    feat(backend): Import InterviewReminder to __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -8,3 +8,4 @@ from app.models.application import Application  # noqa: F401
 from app.models.interview import Interview  # noqa: F401
 from app.models.document import Document  # noqa: F401
 from app.models.contact import Contact  # noqa: F401
+from app.models.interview_reminder import InterviewReminder  # noqa: F401
