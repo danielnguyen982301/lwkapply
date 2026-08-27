913b482bc65feabd642a8d4be4b5dd2f0bae8243
Daniel <danielnguyen982301@gmail.com>
2026-08-27 07:47:38 +0700

    enhance(web): Make top bar sticky

--- webapp/src/layouts/AppLayout.vue
+++ webapp/src/layouts/AppLayout.vue
@@ -148,7 +148,9 @@ const userMenuItems: MenuItem[] = [
     </Menu>
 
     <div class="flex min-w-0 flex-1 flex-col">
-      <header class="flex items-center justify-end border-b border-slate/10 bg-white px-6 py-3">
+      <header
+        class="sticky top-0 flex items-center justify-end border-b border-slate/10 bg-white px-6 py-3"
+      >
         <NotificationBell />
       </header>
 
